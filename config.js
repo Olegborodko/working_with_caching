a96d18da60b173e7a7c2bbc9ba24937eb53ef38a
Apple inside <artur@MacBook-Pro-ARTUR.local>
2020-10-26 10:24:35 +0200

    cache memory+

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-  port: process.env.SERVER_PORT || 3000
+  port: process.env.SERVER_PORT || 3000,
+  cacheMemoryTime: process.env.CACHE_MEMORY_TIME || 15
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const config = require('./config')
+const cacheMiddleware = require('./middleware/cacheMiddleware')
 
 const indexRouter = require('./routes/index');
 
@@ -9,6 +10,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(cacheMiddleware(config.cacheMemoryTime));
 
 app.use('/api', indexRouter)
 
