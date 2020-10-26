24ec92c959999b5aa14ac8ecb7696db9f48946e5
Apple inside <artur@MacBook-Pro-ARTUR.local>
2020-10-26 10:50:38 +0200

    fix middleware only for get requests

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(cacheMiddleware(config.cacheMemoryTime));
+app.get('*', cacheMiddleware(config.cacheMemoryTime));
 
 app.use('/api', indexRouter)
 
